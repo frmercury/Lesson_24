19e451ef8117edf6631f5355ff8907b1455dab5c
Ivan <vaniaevp@gmail.com>
2022-02-11 14:11:04 +0200

    HW_24

--- src/test/java/com/ex/api/ApiSuite.java
+++ src/test/java/com/ex/api/ApiSuite.java
@@ -3,6 +3,7 @@ package com.ex.api;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Type;
@@ -26,14 +27,14 @@ public class ApiSuite extends BaseTest {
     public void postUsersTest() {
        Map<String, Object> mapUsers = usersRequests
                .postUsersRequest("11d16dd3d264220f123d1d9bab1a51491ed15f79b1dc290af2024c128235ff8c",
-                       "/users", 201, "{\"name\":\"island22\",\"email\":\"dharma23@GMAIL.COM\",\"gender\":\"female\",\"status\":\"active\"}");
+                       "/users", 201, "{\"name\":\"island22\",\"email\":\"7dharma23@GMAIL.COM\",\"gender\":\"female\",\"status\":\"active\"}");
 
-       String response = (String) mapUsers.get("json");
+       Response response = (Response) mapUsers.get("response");
 
-       JsonObject jsonObject = new Gson().fromJson(response, JsonObject.class);
+       JSONObject jsonObject = new JSONObject(response.asString());
 
        System.out.println("id = " + jsonObject.get("id"));
-       assertThat(jsonObject.get("name")).asString().isEqualTo("\"island22\"");
-       assertThat(jsonObject.get("email")).asString().isEqualTo("\"dharma23@GMAIL.COM\"");
+       assertThat(jsonObject.get("name")).as("Name is incorrect").isEqualTo("island22");
+       assertThat(jsonObject.get("email")).as("Email is incorrect").isEqualTo("7dharma23@GMAIL.COM");
     }
 }
